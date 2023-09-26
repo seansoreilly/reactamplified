ea2cc937582903ad1bd0051b263317d118e60639
Sean O'Reilly <seansoreilly@gmail.com>
2023-09-26 13:53:25 +1000

    added launch.json

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,53 +1,48 @@
-import React, { useState, useEffect } from 'react';
-// import { v4 as uuidv4 } from 'uuid';
+import React, { useState, useEffect, useCallback } from 'react';
 import './Calendar.css';
 import { API, graphqlOperation } from 'aws-amplify';
-// import { getCalendarEvent } from './graphql/queries';
 import { createCalendarEvent } from './graphql/mutations';
 import { listCalendarEvents } from './graphql/queries';
 import { deleteAllEvents } from './utils';
 
-const Calendar = ({ uuid = "c359c11f-0593-4f0f-92c1-7f93d9783bd5" }) => {  // Replace "YourFixedUUIDHere" with your fixed UUID
+const Calendar = ({ uuid = "c359c11f-0593-4f0f-92c1-7f93d9783bd5" }) => {
   const [hourlyBlocks, setHourlyBlocks] = useState({});
   const [isDragging, setIsDragging] = useState(false);
   const [dragValue, setDragValue] = useState(null);
   const [shouldFetchData, setShouldFetchData] = useState(false);
 
-  useEffect(() => {
-    if (!shouldFetchData) return;
-
-    const fetchData = async () => {
-      try {
-        const result = await API.graphql(graphqlOperation(listCalendarEvents));
-        const aggregatedHourlyBlocks = {};
-        if (result.data.listCalendarEvents.items) {
-          result.data.listCalendarEvents.items.forEach(item => {
-            const itemHourlyBlocks = JSON.parse(item.hourlyBlocks);
-            Object.keys(itemHourlyBlocks).forEach(day => {
-              if (!aggregatedHourlyBlocks[day]) {
-                aggregatedHourlyBlocks[day] = {};
-              }
-              Object.keys(itemHourlyBlocks[day]).forEach(hour => {
-                // Assuming 1 is the value for a blocked hour and 0 for free
-                aggregatedHourlyBlocks[day][hour] = aggregatedHourlyBlocks[day][hour] || itemHourlyBlocks[day][hour];
-              });
-            });
+  const fetchData = useCallback(async () => {
+    try {
+      const result = await API.graphql(graphqlOperation(listCalendarEvents));
+      const { items } = result.data.listCalendarEvents;
+      const aggregatedHourlyBlocks = {};
+      items.forEach(item => {
+        const itemHourlyBlocks = JSON.parse(item.hourlyBlocks);
+        Object.keys(itemHourlyBlocks).forEach(day => {
+          aggregatedHourlyBlocks[day] ??= {};
+          Object.keys(itemHourlyBlocks[day]).forEach(hour => {
+            // Assuming 1 is the value for a blocked hour and 0 for free
+            aggregatedHourlyBlocks[day][hour] ??= itemHourlyBlocks[day][hour];
           });
-          setHourlyBlocks(aggregatedHourlyBlocks);
-        }
-      } catch (error) {
-        console.error('Could not fetch calendar data:', error);
-      }
-      setShouldFetchData(false);  // Reset the flag
-    };
+        });
+      });
+      setHourlyBlocks(aggregatedHourlyBlocks);
+    } catch (error) {
+      console.error('Could not fetch calendar data:', error);
+    }
+    setShouldFetchData(false);  // Reset the flag
+  }, []);
 
-    fetchData();
-  }, [shouldFetchData]);
+  useEffect(() => {
+    if (shouldFetchData) {
+      fetchData();
+    }
+  }, [shouldFetchData, fetchData]);
 
-  const handleMouseDown = async (day, hour) => {
+  const handleMouseDown = useCallback(async (day, hour) => {
     setIsDragging(true);
     const newHourlyBlocks = { ...hourlyBlocks };
-    const dayData = newHourlyBlocks[day] || {};
+    const dayData = newHourlyBlocks[day] ?? {};
     dayData[hour] = !dayData[hour];
     newHourlyBlocks[day] = dayData;
     setHourlyBlocks(newHourlyBlocks);
@@ -58,30 +53,29 @@ const Calendar = ({ uuid = "c359c11f-0593-4f0f-92c1-7f93d9783bd5" }) => {  // Re
         uuid: uuid,
         hourlyBlocks: JSON.stringify(newHourlyBlocks),
       };
-      const result = await API.graphql(graphqlOperation(createCalendarEvent, { input }));  // Changed this line
+      const result = await API.graphql(graphqlOperation(createCalendarEvent, { input }));
       console.log('Successfully created in DynamoDB:', result);
     } catch (error) {
       console.error('Could not create in DynamoDB:', error);
     }
-  };
-  
+  }, [hourlyBlocks, uuid]);
 
-  const handleMouseUp = () => {
+  const handleMouseUp = useCallback(() => {
     setIsDragging(false);
-  };
+  }, []);
 
-  const handleMouseEnter = (day, hour) => {
+  const handleMouseEnter = useCallback((day, hour) => {
     if (isDragging) {
       const newHourlyBlocks = { ...hourlyBlocks };
-      const dayData = newHourlyBlocks[day] || {};
+      const dayData = newHourlyBlocks[day] ?? {};
       dayData[hour] = dragValue;
       newHourlyBlocks[day] = dayData;
       setHourlyBlocks(newHourlyBlocks);
     }
-  };
+  }, [hourlyBlocks, isDragging, dragValue]);
 
   const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const hours = Array.from({ length: 24 }, (_, i) => i);
+  const hours = useState(Array.from({ length: 24 }, (_, i) => i))[0];
 
   return (
     <div className="calendar" onMouseUp={handleMouseUp}>
@@ -95,11 +89,11 @@ const Calendar = ({ uuid = "c359c11f-0593-4f0f-92c1-7f93d9783bd5" }) => {  // Re
       </div>
       {hours.map(hour => (
         <div key={hour} className="calendar-row">
-          <div className="calendar-cell">{`${hour}:00`}</div>
+          <div className="calendar-cell" key={`hour-${hour}`}>{`${hour}:00`}</div>
           {days.map(day => (
             <div
-              key={day}
-              className={`calendar-cell ${hourlyBlocks[day] && hourlyBlocks[day][hour] ? 'selected' : ''}`}
+              key={`${day}-${hour}`}
+              className={`calendar-cell ${hourlyBlocks[day]?.[hour] ? 'selected' : ''}`}
               onMouseDown={() => handleMouseDown(day, hour)}
               onMouseEnter={() => handleMouseEnter(day, hour)}
             ></div>
