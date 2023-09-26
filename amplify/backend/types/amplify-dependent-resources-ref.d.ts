37f0e2c15c2c71bf9197d14a462e7614de85915d
Sean O'Reilly <seansoreilly@gmail.com>
2023-09-26 13:34:19 +1000

    fixed connection

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "reactamplified": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
