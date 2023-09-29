ffd19bc6629a7efb728b4612ea34e97f47ad4c34
Sean O'Reilly <seansoreilly@gmail.com>
2023-09-30 09:38:00 +1000

    Added time dropdowns

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
-import "./Calendar.css";
-import { Button } from "@aws-amplify/ui-react";
+import "./style/Calendar.css";
+import { Button, Card, useTheme, SelectField } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import { API, graphqlOperation } from "aws-amplify";
 import { createCalendarEvent } from "./graphql/mutations";
 import { listCalendarEvents } from "./graphql/queries";
-import { deleteAllEvents } from "./utils";
-import { Card, useTheme } from "@aws-amplify/ui-react";
+import { deleteAllEvents } from "./utils/deleteUtils";
+import ColorPicker from "./ui-components/ColorPicker";
 
 const Calendar = () => {
   const { uuid } = useParams();
@@ -15,11 +15,13 @@ const Calendar = () => {
   const [isDragging, setIsDragging] = useState(false);
   const [dragValue, setDragValue] = useState(null);
   const [shouldFetchData, setShouldFetchData] = useState(false);
-  const [selectedColor, setSelectedColor] = useState("#FF5733"); // Default color
+  const [selectfieldedColor, setSelectFieldedColor] = useState("#FF5733"); // Default color
   const theme = useTheme(); // <-- Add this line
   const handleColorChange = (e) => {
-    setSelectedColor(e.target.value);
+    setSelectFieldedColor(e.target.value);
   };
+  const [startTime, setStartTime] = useState(0); // <-- Add this state
+  const [endTime, setEndTime] = useState(23); // <-- Add this state
 
   const fetchData = useCallback(async () => {
     try {
@@ -63,12 +65,15 @@ const Calendar = () => {
 
       // If it's a boolean, convert it to an object
       if (typeof currentBlock === "boolean") {
-        currentBlock = { selected: currentBlock, color: selectedColor };
+        currentBlock = {
+          selectfielded: currentBlock,
+          color: selectfieldedColor,
+        };
       }
 
-      // Toggle selected state and set color
-      currentBlock.selected = !currentBlock.selected;
-      currentBlock.color = selectedColor;
+      // Toggle selectfielded state and set color
+      currentBlock.selectfielded = !currentBlock.selectfielded;
+      currentBlock.color = selectfieldedColor;
 
       dayData[hour] = currentBlock;
       newHourlyBlocks[day] = dayData;
@@ -88,8 +93,8 @@ const Calendar = () => {
         console.error("Could not create in DynamoDB:", error);
       }
     },
-    [hourlyBlocks, uuid, selectedColor]
-  ); // Note: added selectedColor dependency
+    [hourlyBlocks, uuid, selectfieldedColor]
+  ); // Note: added selectfieldedColor dependency
 
   const handleMouseUp = useCallback(() => {
     setIsDragging(false);
@@ -126,11 +131,36 @@ const Calendar = () => {
         <Button onClick={() => deleteAllEvents(String(uuid))}>
           Delete All Events
         </Button>
-        <input
-          type="color"
-          value={selectedColor}
-          onChange={handleColorChange}
+        <ColorPicker
+          color={selectfieldedColor}
+          onColorChange={handleColorChange}
         />
+
+        {/* Add these lines to show the dropdowns for start and end times */}
+        <SelectField
+          placeholder="Start Time"
+          onChange={(e) => setStartTime(e.target.value)}
+          defaultValue={startTime}
+        >
+          {hours.map((hour) => (
+            <option key={hour} value={hour}>
+              {`${hour}:00`}
+            </option>
+          ))}
+        </SelectField>
+
+        <SelectField
+          placeholder="End Time"
+          onChange={(e) => setEndTime(e.target.value)}
+          defaultValue={endTime}
+        >
+          {hours.map((hour) => (
+            <option key={hour} value={hour}>
+              {`${hour}:00`}
+            </option>
+          ))}
+        </SelectField>
+
         <div className="calendar-row header">
           <div className="calendar-cell"></div>
           {days.map((day) => (
@@ -139,37 +169,39 @@ const Calendar = () => {
             </div>
           ))}
         </div>
-        {hours.map((hour) => (
-          <div key={hour} className="calendar-row">
-            {/* Add this line to show the times in the left column */}
-            <div
-              className="calendar-cell"
-              key={`hour-${hour}`}
-            >{`${hour}:00`}</div>
+        {hours.map((hour) =>
+          hour >= startTime && hour <= endTime ? (
+            <div key={hour} className="calendar-row">
+              {/* Add this line to show the times in the left column */}
+              <div
+                className="calendar-cell"
+                key={`hour-${hour}`}
+              >{`${hour}:00`}</div>
 
-            {days.map((day) => {
-              const block = hourlyBlocks[day]?.[hour] ?? {};
-              return (
-                <div
-                  key={`${day}-${hour}`}
-                  className={`calendar-cell ${
-                    block.selected ? "selected" : ""
-                  }`}
-                  style={{
-                    backgroundColor: block.selected
-                      ? block.color
-                      : (theme?.colors?.background?.primary?.value || ''),
-                    borderRadius: theme?.borderRadius?.default?.value || '',
-                    fontSize: theme?.fontSizes?.body?.value || '',
-                    fontWeight: theme?.fontWeights?.regular?.value || '',
-                  }}
-                  onMouseDown={() => handleMouseDown(day, hour)}
-                  onMouseEnter={() => handleMouseEnter(day, hour)}
-                ></div>
-              );
-            })}
-          </div>
-        ))}
+              {days.map((day) => {
+                const block = hourlyBlocks[day]?.[hour] ?? {};
+                return (
+                  <div
+                    key={`${day}-${hour}`}
+                    className={`calendar-cell ${
+                      block.selectfielded ? "selectfielded" : ""
+                    }`}
+                    style={{
+                      backgroundColor: block.selectfielded
+                        ? block.color
+                        : theme?.colors?.background?.primary?.value || "",
+                      borderRadius: theme?.borderRadius?.default?.value || "",
+                      fontSize: theme?.fontSizes?.body?.value || "",
+                      fontWeight: theme?.fontWeights?.regular?.value || "",
+                    }}
+                    onMouseDown={() => handleMouseDown(day, hour)}
+                    onMouseEnter={() => handleMouseEnter(day, hour)}
+                  ></div>
+                );
+              })}
+            </div>
+          ) : null
+        )}
       </Card>
     </div>
   );
